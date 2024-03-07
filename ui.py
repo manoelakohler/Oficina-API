3ab0e09c066cfd2e84bb165298d3ead4f2658841
Manoela <manoelakohler@puc-rio.br>
2024-03-07 11:22:29 -0300

    user interface funcionando

--- ui.py
+++ ui.py
@@ -1,70 +1,59 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 import requests
+from io import StringIO
 import base64
-import pandas as pd
-import plotly.express as px
-from io import BytesIO
 
-app_dash = dash.Dash(__name__)
+app = dash.Dash(__name__)
 
-app_dash.layout = html.Div([
+app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
-        children=html.Div([
-            'Arraste e solte ou ',
-            html.A('selecione um arquivo CSV')
-        ]),
+        children=html.Div(['Arraste e solte ou ', html.A('selecione um arquivo CSV')]),
         multiple=False
     ),
-    html.Div(id='output-data-upload'),
+    html.Button('Fazer Previsões', id='btn-predict'),
+    html.Div(id='output-predictions')
 ])
 
 
-def parse_contents(contents):
-    # Decodifica o conteúdo base64 diretamente em um DataFrame
+def call_api(file_contents):
+    url = 'https://bimaster-c4c1bb7dcb68.herokuapp.com/predict'
+    files = {'file': ('filename.csv', file_contents)}
+    response = requests.post(url, files=files)
+    return response.json()
+
+
+@app.callback(
+    Output('output-predictions', 'children'),
+    [Input('btn-predict', 'n_clicks')],
+    [dash.dependencies.State('upload-data', 'contents')]
+)
+def update_predictions(n_clicks, contents):
+    if n_clicks is None:
+        raise PreventUpdate
+
+    if contents is None:
+        return 'Nenhum arquivo carregado.'
+
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
-    df = pd.read_csv(BytesIO(decoded), encoding='utf-8')
-    return df
+    file_contents = StringIO(decoded.decode('utf-8'))
 
+    api_response = call_api(file_contents)
 
-@app_dash.callback(Output('output-data-upload', 'children'),
-                   Input('upload-data', 'contents'))
-def update_output(contents):
-    if contents is None:
-        return
+    predictions = api_response.get('Predictions', [])
 
-    df = parse_contents(contents)
+    if not predictions:
+        return 'Nenhuma previsão disponível.'
 
-    # Chame a API FastAPI para obter previsões
-    response = requests.post("http://localhost:5000/predict", files={"file": contents})
-    response = requests.post("http://localhost:5000/#/default/predict_predict_post", files={"file": contents})
-
-    predictions = response.json()["Predictions"]
-
-    # Crie a tabela com os resultados
-    table = html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in df.columns] + [html.Th("Prediction")])] +
-        # Body
-        [html.Tr([html.Td(df.iloc[i, col]) for col in range(len(df.columns))] + [html.Td(predictions[i])]) for i in
-         range(len(df))]
-    )
-
-    # Crie o gráfico de pizza
-    pie_chart = px.pie(names=["M", "B", "S"],
-                       values=[predictions.count("M"), predictions.count("B"), predictions.count("S")],
-                       title='Prediction Distribution')
-
-    return [
-        html.H5("Resultados da previsão"),
-        table,
-        dcc.Graph(figure=pie_chart)
-    ]
+    return html.Div([
+        html.Div('Previsões:'),
+        html.Pre(', '.join(predictions))
+    ])
 
 
 if __name__ == '__main__':
-    app_dash.run_server(debug=True)
+    app.run_server(debug=True)
